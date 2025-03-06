ee4bfeea9a8c87828b419d67c7c49151f85720ac
Sandhya17122004 <sandhyasakthi753@gmail.com>
2025-03-06 14:38:46 +0530

    Add files via upload

---
+++ src/context/GeneralContext.jsx
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+
+export const GeneralContext = React.createContext();
+
+const GeneralContextProvider = ({children}) => {
+
+    const [topNews, setTopNews] = useState([])
+
+    const [businessNews, setBusinessNews] = useState([]);
+    const [technologyNews, setTechnologyNews] = useState([]);
+    const [politicsNews, setPoliticsNews] = useState([]);
+
+    const newkey=" 5ee74a8247d34fe6a992405a5218f21c "
+    useEffect(() => { 
+        fetchTopNews() 
+        fetchBusinessNews()
+        fetchPoliticsNews()
+        fetchTechnologyNews()
+      }, [])
+    
+      const fetchTopNews = async () => {
+        try {
+          const response = await axios.get('https://newsapi.org/v2/everything?q=popular&apiKey=5ee74a8247d34fe6a992405a5218f21c');
+          setTopNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      const fetchBusinessNews = async () => {
+        try {
+          const response = await axios.get('https://newsapi.org/v2/everything?q=business&apiKey=5ee74a8247d34fe6a992405a5218f21c');
+          setBusinessNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      const fetchPoliticsNews = async () => {
+        try {
+          const response = await axios.get('https://newsapi.org/v2/everything?q=politics&apiKey=5ee74a8247d34fe6a992405a5218f21c');
+          setPoliticsNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      const fetchTechnologyNews = async () => {
+        try {
+          const response = await axios.get('https://newsapi.org/v2/everything?q=technology&apiKey=5ee74a8247d34fe6a992405a5218f21c');
+          setTechnologyNews(response.data.articles);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+    
+  return (
+    <GeneralContext.Provider value={{topNews, businessNews, technologyNews, politicsNews}} >{children}</GeneralContext.Provider>
+  )
+}
+
+export default GeneralContextProvider
